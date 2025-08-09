0e3eca54c50ca0c6c36d05ce6172ea06a5aba824
hadjbakir <hadjbakirbvb.16@gmail.com>
2025-08-09 12:11:50 +0100

    old next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,64 +1,39 @@
-/* @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: {
-      allowedOrigins: [
-        "https://easycom.rakmana-dz.com:1000",
-        "http://easycom.rakmana-dz.com:1000",
-      ],
-    },
-  },
   basePath: process.env.BASEPATH,
   images: {
+    domains: ['localhost'],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'easycom.rakmana-dz.com',
-        port: '1000',
-        pathname: '/storage/',
-      },
-    ],
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/storage/**'
+      }
+    ]
   },
-  async headers() {
+  redirects: async () => {
     return [
       {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "https://easycom.rakmana-dz.com" },
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE" },
-          { key: "Access-Control-Allow-Headers", value: "Authorization, Content-Type" },
-        ],
+        source: '/',
+        destination: '/en/front-pages/landing-page',
+        permanent: true,
+        locale: false
       },
-    ];
-  },
-  async rewrites() {
-    return [
       {
-        source: "/api/:path*",
-        destination: "http://easycom.rakmana-dz.com:1000/api/:path*",
+        source: '/:lang(en|fr|ar)',
+        destination: '/:lang/front-pages/landing-page',
+        permanent: true,
+        locale: false
       },
-    ];
-  },
-  redirects: async () => [
-    {
-      source: '/',
-      destination: '/en/front-pages/landing-page',
-      permanent: true,
-      locale: false,
-    },
-    {
-      source: '/:lang(en|fr|ar)',
-      destination: '/:lang/front-pages/landing-page',
-      permanent: true,
-      locale: false,
-    },
-    {
-      source: '/((?!(?:en|fr|ar|front-pages|favicon.ico)\\b)):path',
-      destination: '/en/:path',
-      permanent: true,
-      locale: false,
-    },
-  ],
-};
+      {
+        source: '/((?!(?:en|fr|ar|front-pages|favicon.ico)\\b)):path',
+        destination: '/en/:path',
+        permanent: true,
+        locale: false
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
